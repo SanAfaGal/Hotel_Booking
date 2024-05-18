a12655e7f1fcfbccc46cb1d6ab74207748d32759
Santiago Afanador Galeano <afanadorgaleano@gmail.com>
2024-05-18 10:38:57 -0500

    Refactorizar DapperContext

--- Hotel_Booking/Context/DapperContext.cs
+++ Hotel_Booking/Context/DapperContext.cs
@@ -6,10 +6,7 @@ namespace Hotel_Booking.Context
     public class DapperContext
 
     {
-
-        private readonly IConfiguration _configuration;
-
-        private readonly string _connectionString;
+        private readonly string? _connectionString;
 
         public DapperContext(IConfiguration configuration)
         {
